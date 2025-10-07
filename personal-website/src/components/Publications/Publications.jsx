b7d10267179b1fc520b99b5b007219c4ab3b7728
Natália <smachadonatalia@gmail.com>
2025-10-07 12:14:14 -0300

    adjusment to blog post formatting

--- personal-website/src/components/Publications/Publications.jsx
+++ personal-website/src/components/Publications/Publications.jsx
@@ -4,7 +4,7 @@ import './Publications.css'
 function Publications() {
     return <>
         {publications.map(pub => (
-            <a href={pub.doi} className="card">
+            <a href={pub.url} className="card">
                 <h1>{pub.title}</h1>
                 <h2>Publicado em {pub.publisher}</h2>
                 <p>{pub.doi}</p>
--- personal-website/src/publications.jsx
+++ personal-website/src/publications.jsx
@@ -1,18 +1,21 @@
 const publications = [
     {
-        "title": "Identificação de requisitos de acordo com a Lei Geral de Proteção de Dados: estratégias de uso para a ferramenta IdeiaWare",
-        "publisher": "Revista Conjecturas",
-        "doi": "https://doi.org/10.53660/CONJ-1755-2R72"
-    },
-    {
         "title": "Proposição de Requisitos Baseados na Lei Geral de Proteção de Dados para a Ferramenta IdeiaWare",
         "publisher": "III Mostra de Extensão, Ciência e Tecnologia da Unisc",
-        "doi": ""
+        "doi": "",
+        "url": "https://online.unisc.br/acadnet/anais/index.php/mostraextensaounisc/article/view/22709"
+    },
+    {
+        "title": "Identificação de requisitos de acordo com a Lei Geral de Proteção de Dados: estratégias de uso para a ferramenta IdeiaWare",
+        "publisher": "Revista Conjecturas",
+        "doi": "https://doi.org/10.53660/CONJ-1755-2R72",
+        "url": "https://conjecturas.org/index.php/edicoes/article/view/1755/"
     },
     {
         "title": "Violações à Lei Geral de Proteção de Dados e Proposição de Requisitos para a Ferramenta IdeiaWare",
         "publisher": "Revista Jovens Pesquisadores",
-        "doi": "https://doi.org/10.17058/rjp.v13i1.18462"
+        "doi": "https://doi.org/10.17058/rjp.v13i1.18462",
+        "url": "https://seer.unisc.br/index.php/jovenspesquisadores/article/view/18462"
     }
 ];
 
