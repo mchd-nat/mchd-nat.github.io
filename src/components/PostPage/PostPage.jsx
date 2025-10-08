3cf61eb6e162e702e59d10f5472ca195827f670e
Natália <smachadonatalia@gmail.com>
2025-10-08 00:41:25 -0300

    simplifying the project tree

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
-import blogPosts from "../../blogPosts";
-import Slugify from "../../slugify";
+import blogPosts from "../../blogPosts.jsx";
+import Slugify from "../../slugify.jsx";
 import "./PostPage.css";
 import { marked } from "marked";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle.jsx";
