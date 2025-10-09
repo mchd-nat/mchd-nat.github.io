c9e16b41fa3eb1e105e9b33a13ad44039f2f2760
Natália <smachadonatalia@gmail.com>
2025-10-08 22:33:03 -0300

    updating i18n

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -1,13 +1,18 @@
 import publications from "../../publications.jsx";
 import "./Publications.css";
+import { useTranslation } from "react-i18next";
 
 function Publications() {
+  const { t } = useTranslation();
+
   return (
     <>
       {publications.map((pub) => (
         <a key={pub.url} href={pub.url} target="_blank" className="card">
           <h1>{pub.title}</h1>
-          <h2>Publicado em {pub.publisher}</h2>
+          <h2>
+            {t("publisher")} {pub.publisher}
+          </h2>
           <p>
             <span>{pub.doi}</span>
           </p>
